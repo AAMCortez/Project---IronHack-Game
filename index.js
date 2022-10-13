19b0fbc731dd4d2e52acf606ed1f17f1899e6608
António Cortez <aamcortez@gmail.com>
2022-10-13 21:55:49 +0100

    Platforms done

--- index.js
+++ index.js
@@ -1,31 +1,77 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-function updateCanvas() {
-    drawPlatforms();
- }
-
-class Ball {}
-
-class Platforms {
-   constructor(width, height, color, x, y) {
-      this.width = width;
-      this.height = height;
-      this.color = color;
-      this.x = x;
-      this.y = y;
-   }
-
-   draw() {
+let ball = {
+   x: 475,
+   y: 675,
+   vx: 1,
+   vy: 2,
+   userPull: 0,
+   radius: 25,
+   color: "green",
+   draw: function () {
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
       context.fillStyle = this.color;
-      context.fillRect(this.x, this.y, this.width, this.height);
+      context.fill();
+   },
+};
+
+const platforms = [];
+
+const gameArea = {};
+
+// Canvas boundaries and platform contact with the ball
+let gravity = 0.5;
+function hitBottom() {
+   let rockbottom = canvas.height - ball.radius;
+   if (ball.y > rockbottom) {
+      ball.y = rockbottom;
    }
 }
-const platforms = [];
-const platform1 = new Platforms(100, 10, "red", 250, 600);
-const platform2 = new Platforms(25, 10, "red", 350, 350);
-const platform3 = new Platforms(50, 10, "red", 150, 500);
-platforms.push(platform1, platform2, platform3);
+function update() {
+   context.clearRect(0, 0, canvas.width, canvas.height);
+   hitBottom();
+   ball.vy = ball.vy + (gravity - ball.userPull);
+   ball.y += ball.vy;
+   console.log(ball.y);
+
+   ball.draw();
+   requestAnimationFrame(update);
+}
+document.onkeydown = function (e) {
+   if (e.keyCode == 32) {
+      ball.userPull = 0.3;
+   }
+};
+
+document.onkeyup = function (e) {
+   if (e.keyCode == 32) {
+      ball.userPull = 0;
+   }
+};
+
+function startGame() {
+   canvas.removeAttribute("hidden");
+
+   const platform1 = new Platform(200, 10, 50, 600);
+   const platform2 = new Platform(150, 10, 250, 500);
+   const platform3 = new Platform(100, 10, 400, 400);
+   const platform4 = new Platform(75, 10, 250, 300);
+   const platform5 = new Platform(50, 10, 50, 200);
+   const platform6 = new Platform(25, 10, 300, 100);
+   platforms.push(
+      platform1,
+      platform2,
+      platform3,
+      platform4,
+      platform5,
+      platform6
+   );
+   drawPlatforms();
+   ball.draw();
+   updateCanvas();
+}
 
 function drawPlatforms() {
    platforms.forEach((platform) => {
@@ -33,8 +79,9 @@ function drawPlatforms() {
    });
 }
 
-const gameArea = {};
+function updateCanvas() {
+   drawPlatforms();
+   requestAnimationFrame(updateCanvas);
+}
 
-
-canvas.removeAttribute("hidden")
- updateCanvas()
+startGame();
